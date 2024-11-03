29f8a1bbe1e1b263d557d383b5549e565ddc71cb
Amekyras <ash.skeet@pobox.com>
2024-11-03 22:51:10 +0000

    attempted asyncio badly

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,7 +1,7 @@
 import micropython
 import utime
 from machine import Pin, PWM
-#import asyncio
+import asyncio
 
 # button pins
 
@@ -12,45 +12,64 @@ class box:
         pass
 
 
+async def flash(pin, period, decay=0):
+    cycles = 0
+    #Pin object connected to LED
+
+    while True:
+        if period == 0:
+            break
+
+        pin.toggle()
+        print(f"Flashing {pin}")
+        await asyncio.sleep(period)
+
+        if decay != 0:
+            cycles +=1
+            if decay >= cycles:
+                break
+    return()
+
+
 buzzer = PWM(Pin(13), freq=2500, duty_u16=0)
 
 
-def buzz(speaker):
+async def buzz(speaker):
     #speaker.duty_u16(50000)
 
     speaker.duty_u16(50000)
     speaker.freq(900)
 
     speaker.freq(300)
-    utime.sleep_ms(125)
+    await asyncio.sleep(0.125)
 
     speaker.duty_u16(0)
-    utime.sleep_ms(25)
+    await asyncio.sleep(0.25)
     speaker.duty_u16(50000)
 
     speaker.freq(750)
-    utime.sleep_ms(125)
+    await asyncio.sleep(0.125)
     
     speaker.duty_u16(0)
-    utime.sleep_ms(25)
+    await asyncio.sleep(0.25)
     speaker.duty_u16(50000)
     
     speaker.freq(300)
-    utime.sleep_ms(125)
+    await asyncio.sleep(0.125)
     
     speaker.duty_u16(0)
-    utime.sleep_ms(25)
+    await asyncio.sleep(0.25)
     speaker.duty_u16(50000)
 
     speaker.freq(750)
-    utime.sleep_ms(125)
+    await asyncio.sleep(0.125)
     
     speaker.duty_u16(0)
-    utime.sleep_ms(25)
+    await asyncio.sleep(0.25)
     speaker.duty_u16(50000)
 
     speaker.freq(300)
-    utime.sleep_ms(125)
+    await asyncio.sleep(0.125)
 
     speaker.duty_u16(0)
     return()
@@ -59,7 +78,7 @@ def buzz(speaker):
 
 
 
-status_led = Pin("gpio25", Pin.OUT)
+status_led = Pin(25, Pin.OUT)
 control = box(14, 15)
 
 button_pins = [2, 4, 6, 8, 16, 18, 20, 26]
@@ -71,35 +90,47 @@ for i in range(0, len(button_pins)):
     boxes.append(box(button_pin=button_pins[i], led_pin=led_pins[i]))
 
 lock = False
-pulse = 0
+#pulse = 0
 
-while True:
-    if not lock:
-        control.led.on()
-        for i in boxes:
-            if i.button.value() == 1:
-                lock = True
-                i.led.on()
-                control.led.off()
-                buzz(speaker=buzzer)
-                break
-        pulse +=1
-        #gc.collect()
-        if pulse % 10000 == 0:
-            for x in boxes:
-                print(x.button.value())
-            status_led.toggle()
-            print (pulse)
 
-    else:
+async def main():
+
+    #wait for control box press to trigger
+    lock = True
+    control.led.off()
+    while True:
         if control.button.value() == 1:
-            print("Resetting")
-            lock = False
-            pulse = 0
-            
-            for i in boxes:
-                i.led.off()
             control.led.on()
+            flashtask = asyncio.create_task(flash(status_led, period=1))
+            lock = False
+            break
+
+    while True:
+        await asyncio.sleep(0)
+        if not lock:
+            #control.led.on()
+
+            for i in boxes:
+                if i.button.value() == 1:
+                    lock = True
+                    i.led.on()
+                    control.led.off()
+                    asyncio.run(buzz(speaker=buzzer))
+                    flashtask.cancel()
+                    break
 
         
+
+        else:
+            if control.button.value() == 1:
+                print("Resetting")
+                lock = False
+                pulse = 0
+                
+                for i in boxes:
+                    i.led.off()
+                control.led.on()
+                flashtask = asyncio.create_task(flash(status_led, 1))
+
+asyncio.run(main())      
     

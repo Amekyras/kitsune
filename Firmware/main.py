410a0100e275dc15c2f9b083d919ae320852da83
Amekyras <ash.skeet@pobox.com>
2024-12-30 15:13:17 +0000

    drastically cleaned up code, removed neotimer

--- Firmware/main.py
+++ Firmware/main.py
@@ -3,15 +3,24 @@ import micropython
 from machine import *
 from neopixel import NeoPixel
 from buzzer_music import music
-from neotimer import Neotimer
 import rp2
 
+buzzer_pin = 13
+status_pin = 25
+control_led = 15
+control_pin = 14
+pixel_pin = 23
 
+switch_pins = [28, 23, 22, 12, 11, 10] #v0.2 pins
+switch_ids = ["switch1", "switch2", "switch3", "switch4", "switch5", "switch6"]
 
+button_pins = [2, 4, 6, 8, 16, 18, 20, 26] #v0.2 pins
+led_pins = [3, 5, 7, 9, 17, 19, 21, 27] #v0.2 pins
+ids = ["A1", "A2", "A3", "A4", "B4", "B3", "B2", "B1"]
 
 # starting variable values
 lock = True
-pulse = 0
+#pulse = 0
 flag = False
 active = None
 
@@ -59,15 +68,12 @@ class box:
 
 buzzer = PWM(Pin(13), freq=2500, duty_u16=0)
 
-pixel_pin = Pin(23, Pin.OUT)
 
-pixel = NeoPixel(pixel_pin, 1)
-pixel.fill((0, 0, 0))
-pixel.write()
+
 
 def buzz(speaker):
     #speaker.duty_u16(50000)
-    if not switches[1].button.value():
+    if not mute:
         speaker.duty_u16(50000)
         speaker.freq(900)
 
@@ -120,44 +126,74 @@ def flash_pixel():
     #pixel_timer.init(mode=Timer.ONE_SHOT, period=1000, callback=flash_pixel(toggle))
 
 
+def bundle_handler(mode):
+    # respond to raised flag
+    status_timer.deinit()
+    global lock
+    global flag
+    lock = True
+    flag = False
+    control.led.off() # type: ignore
+    active.led.on() # type: ignore
+    pixel.fill((255, 0, 0))
+    pixel.write()
+    buzz(speaker=buzzer)
+    pass
 
-buzzer = PWM(Pin(13), freq=2500, duty_u16=0)
 
-pixel_pin = Pin(23, Pin.OUT)
-pixel = NeoPixel(pixel_pin, 1)
-pixel.fill((0, 0, 0))
-pixel.write()
+def reset_handler(mode):
+    print("Resetting")
+    global lock
+    lock = False
+    status_timer.init(period=1000, callback=status_toggle)
 
-status_led = Pin(25, Pin.OUT)
+    
+    for i in boxes:
+        i.led.off()
+    control.led.on() # type: ignore
+    pixel.fill((0, 0, 0))
+    pixel.write()
+
+
+#initialise hardware
+buzzer = PWM(Pin(buzzer_pin), freq=2500, duty_u16=0)
+
+status_led = Pin(status_pin, Pin.OUT)
 status_led.off()
+status_timer = Timer()
 
-control = box(14, led_pin=15, id="Control")
+def status_toggle(t):
+    status_led.toggle()
+
+
+control = box(control_pin, led_pin=control_led, id="Control")
 control.led.off() # type: ignore
 
-
-
-
-
-
-
+boxes = []
+for i in range(0, len(button_pins)):
+    boxes.append(box(button_pin=button_pins[i], led_pin=led_pins[i], id=ids[i], irq=True))
 
 
 ### SWITCHBOARD ###
-
 # 1 - DEBUG
-# 2 - MUTE
+# 2 - MUTE (must disable neopixel to read)
 # 3 - EGG
 # 4 - 
 # 5 - BRANCH
 # 6 - ACTIVATE MULTIBUZZER
 switches = []
 
-switch_pins = [28, 23, 22, 12, 11, 10] #v0.2 pins
-switch_ids = ["switch1", "switch2", "switch3", "switch4", "switch5", "switch6"]
-
 for i in range(0, len(switch_pins)):
     switches.append(box(button_pin=switch_pins[i], id=switch_ids[i], pull="up"))
 
+if not switches[1].button.value():
+    mute = True
+
+
+#initialise neopixel after mute check
+pixel = NeoPixel(Pin(pixel_pin), 1)
+pixel.fill((0, 0, 0))
+pixel.write()
 
 
 ### EASTER EGG DETECTION/EXECUTION ###
@@ -174,28 +210,16 @@ def egg(songfile):
         utime.sleep(0.04)
 
 
-if not switches[2].button.value():
+if not switches[2].button.value() and not mute:
     egg(song)
 ### END EGG ###
-
-
-button_pins = [2, 4, 6, 8, 16, 18, 20, 26] #v0.1 pins
-led_pins = [3, 5, 7, 9, 17, 19, 21, 27] #v0.1 pins
-ids = ["A1", "A2", "A3", "A4", "B4", "B3", "B2", "B1"]
-
-boxes = []
-
-for i in range(0, len(button_pins)):
-    boxes.append(box(button_pin=button_pins[i], led_pin=led_pins[i], id=ids[i], irq=True))
-
-
 
 
 
 #startup sound
 jingletrack = "0 G5 1 15 0.5039370059967041;1 F#5 1 15 0.5039370059967041;3 E5 3 15 0.5039370059967041;6 F#5 2 15 0.5039370059967041"
 jingle = music(jingletrack, pin=Pin(13), looping=False)
-if not switches[1].button.value():
+if not mute:
     while True:
         jingle.tick()
         utime.sleep(0.04)
@@ -204,11 +228,14 @@ if not switches[1].button.value():
             break
 buzzer.duty_u16(0) #kill buzzer
 
+
 print("Entering setup loop")
 while True: 
     #setup check
 
     if not switches[0].button.value():
+        pixel.fill((0, 255, 0))
+        pixel.write()
         
         #buzzer test loop
         testboxes = []
@@ -244,42 +271,24 @@ if not switches[5].button.value():
         role = "Branch"
     else:
         role = "Main"
+else:
+    role = None
 
 #main loop
+status_timer.init(period=1000, callback=status_toggle)
 print("Entering main loop")
 while True:
     if not lock:
         control.led.on() # type: ignore
         if flag:
-            lock = True
-            flag = False
-            control.led.off() # type: ignore
-            active.led.on() # type: ignore
-            pixel.fill((255, 0, 0))
-            pixel.write()
-            buzz(speaker=buzzer)
-            
-        pulse +=1
-        if pulse % 10000 == 0:
-            for x in boxes:
-                print(x.button.value())
-            status_led.toggle()
-            print (pulse)
+            bundle_handler(role)
 
     else:
         if control.button.value() == 1:
-            print("Resetting")
-            lock = False
-            pulse = 0
-            
-            for i in boxes:
-                i.led.off()
-            control.led.on() # type: ignore
-            pixel.fill((0, 0, 0))
-            pixel.write()
+            reset_handler(role)
 
 
 
 
         
-    
+     

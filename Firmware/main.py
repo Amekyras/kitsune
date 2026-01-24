b31f1b405724e046e1af6e00bf730883251bc146
Aisling Skeet <ash.skeet@pobox.com>
2026-01-24 11:58:58 +0000

    fixed irq debug thing

--- Firmware/main.py
+++ Firmware/main.py
@@ -8,11 +8,15 @@ import os
 from lib.line_clear import clear_line
 import micropython
 import sys
+import io
 
 micropython.alloc_emergency_exception_buf(100)
+
+e = None
+
 try:
 
-# 1. Setup
+    # 1. Setup
 
     player_data, switches, mcp = init_hardware() # shim pins, read switches, init mcp
 
@@ -33,7 +37,7 @@ try:
     print ("Pinout:", board_pins["name"])
 
 
-    
+
     engine.buzz_startup()
 
 
@@ -42,50 +46,51 @@ try:
         print("DEBUG MODE ENABLED")
 
         
-
-        while True:
-            on_list = []
-            off_list = []
-            for p in players:
-                if p.button.value() == 1:
-                    on_list.append(p.id)
-                    p.update_led(1)
+        try:
+            for i in players:
+                i.button.irq(handler=None)
+            while True:
+                on_list = []
+                off_list = []
+                for p in players:
+                    if p.button.value() == 1:
+                        on_list.append(p.id)
+                        p.update_led(1)
+                    else:
+                        off_list.append(p.id)
+                        p.update_led(0)
+                if control_button.value() == 1:
+                    on_list.append("CTRL")
+                    control_led.on()
                 else:
-                    off_list.append(p.id)
-                    p.update_led(0)
-            if control_button.value() == 1:
-                on_list.append("CTRL")
-                control_led.on()
-            else:
-                off_list.append("CTRL")
-                control_led.off()
+                    off_list.append("CTRL")
+                    control_led.off()
 
-            for s in switches:
-                if s.value() == 1:
-                    on_list.append(f"Switch {switches.index(s)+1}")
-                else:
-                    off_list.append(f"Switch {switches.index(s)+1}")
-            print(f"ON: {on_list}")
-            print(f"OFF: {off_list}")
-            utime.sleep(0.1)
-            clear_line(2)
+                for s in switches:
+                    if s.value() == 1:
+                        on_list.append(f"Switch {switches.index(s)+1}")
+                    else:
+                        off_list.append(f"Switch {switches.index(s)+1}")
+                print(f"ON: {on_list}, OFF: {off_list}", end="\r")
+                #sys.stdout.flush()
+                #print(f"OFF: {off_list}")
+                #utime.sleep(0.1)
+            #clear_line(2)
 
+        except Exception as e:
 
-
+            buf = io.StringIO()
+            sys.print_exception(e, buf)
+            print(buf.getvalue())
+            #with open("exceptions.txt", "a") as f:
+    #    sys.print_exception(e, f)
 
     print("Unlock to start")
 
-    #while True:
-    #    try:
-    #        engine.buzz()
-    #    except Exception as e:
-    #        cfg.buzzer.duty_u16(0)
-    #        sys.print_exception(e)
-    #        break
 
-    #cfg.buzzer.deinit()
-    #cfg.buzzer_pin.high()
-    # 2. Main Loop
+#cfg.buzzer.deinit()
+#cfg.buzzer_pin.high()
+# 2. Main Loop
     while True:
         # If the MCP is used for buttons, we poll them here
         # If using IRQs on native pins, this loop stays nearly empty!
@@ -97,5 +102,6 @@ try:
 
 except Exception as e:
     #cfg.buzzer.duty_u16(0)
-    sys.print_exception(e)
+    print(repr(e))
+    #sys.print_exception(e)
     #machine.reset()
